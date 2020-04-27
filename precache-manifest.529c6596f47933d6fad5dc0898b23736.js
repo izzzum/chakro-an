25bc1b586524aee296034fb468f5dcfe3e25afa1
izzzum\izum <izum88@gmail.com>
2020-04-27 01:30:43 -0400

    first

---
+++ precache-manifest.529c6596f47933d6fad5dc0898b23736.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b128c1cdf12cd0211cb",
+    "url": "/chakro-an/css/app.80c93388.css"
+  },
+  {
+    "revision": "5c196c73d6e3fa83d9ac",
+    "url": "/chakro-an/css/chunk-vendors.fa19beba.css"
+  },
+  {
+    "revision": "cfd3dce57f28f5955c90fc5b5385d4b3",
+    "url": "/chakro-an/fonts/OpenSansCondensed-Bold.cfd3dce5.ttf"
+  },
+  {
+    "revision": "3589bddbe338e444d408f4dbc545ca1e",
+    "url": "/chakro-an/fonts/OpenSansCondensed-Light.3589bddb.ttf"
+  },
+  {
+    "revision": "5cbe3a855cf32f7afdea428062a6ce01",
+    "url": "/chakro-an/img/bg-bot-left.5cbe3a85.png"
+  },
+  {
+    "revision": "6679a1cd5ad3118e359807c179d66009",
+    "url": "/chakro-an/img/bg-bot-right.6679a1cd.png"
+  },
+  {
+    "revision": "788f276d5f024a1cc02df34ec4120a43",
+    "url": "/chakro-an/img/title-img.788f276d.png"
+  },
+  {
+    "revision": "b90901ba6ddaaeb6d32685949640919e",
+    "url": "/chakro-an/img/yoga-bg.b90901ba.png"
+  },
+  {
+    "revision": "9465587ba6eeb3886b8c0ad618be421f",
+    "url": "/chakro-an/index.html"
+  },
+  {
+    "revision": "1b128c1cdf12cd0211cb",
+    "url": "/chakro-an/js/app.656d5f1f.js"
+  },
+  {
+    "revision": "5c196c73d6e3fa83d9ac",
+    "url": "/chakro-an/js/chunk-vendors.0df2f33a.js"
+  },
+  {
+    "revision": "8105509ea00bd773c23e2a152d57be07",
+    "url": "/chakro-an/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chakro-an/robots.txt"
+  }
+]);
